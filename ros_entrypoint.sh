781d1cf1fbbb6aae42d279037152d41be69c4e2c
fbottega <fbottega@motorola.com>
2020-04-25 21:27:01 -0300

    Fix docker-compose mobilesim network problem

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-cd ~/catkin_ws/
+cd /catkin_ws/
 catkin_make
 source devel/setup.bash
-roscore &
+
+exec "$@"
